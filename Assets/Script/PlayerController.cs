605002c8dd8faad80026a3d6a0a9b30439fae048
souta_notePC <darkwind0831@gmail.com>
2019-03-05 17:42:36 +0900

    カメラのスクリプト半分完成　プレイヤーの動く

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,17 +2,13 @@
 
 public class PlayerController : MonoBehaviour
 {
-    public float Speed = 3.0f;//速度
-    public float JumpPower = 6.0f;//ジャンプパワー
+    public float Speed = 3.0f;
+    public float JumpPower = 6.0f;
 
     Rigidbody rb;
-    Ray ray;
-    RaycastHit hit;
-    bool isGround;
     public Camera Camera;
-    [SerializeField] float ray_distance = 5;
 
-
+    bool AGBuffer = false;
 
     void Start()
     {
@@ -21,37 +17,60 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        RayChack();//レイを管理するやつ
-            var x = Input.GetAxis("Horizontal");
-            var z = Input.GetAxis("Vertical");
-            Vector3 camForward = Vector3.Scale(Camera.transform.forward, new Vector3(1, 0, 1)).normalized;
-            Vector3 moveForward = camForward * z + Camera.transform.right * x;
-            transform.position += moveForward * Speed * Time.deltaTime;
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+        Vector3 camForward = Vector3.Scale(Camera.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 moveForward = camForward * z + Camera.transform.right * x;
+        transform.position += moveForward * Speed * Time.deltaTime;
 
-            if (moveForward.sqrMagnitude > 0.0f) transform.rotation = Quaternion.LookRotation(moveForward);
+        if (moveForward.sqrMagnitude > 0.0f) transform.rotation = Quaternion.LookRotation(moveForward);
 
-        if (isGround)
-        {
-            if (Input.GetButtonDown("Jump")) rb.AddForce(0.0f, JumpPower, 0.0f, ForceMode.Impulse);
-        }
+        if (Input.GetButtonDown("Jump")) rb.AddForce(0.0f, JumpPower, 0.0f, ForceMode.Impulse);
+
+       // if (AGBuffer != IsGround())
+       // {
+            if (IsGround())
+            {
+            rb.velocity = Vector3.zero;
+            Debug.Log("地上にいますわ!");
+            }
+            else
+            {
+                Debug.Log("空中にいますわ!");
+                //落下中なら滑空 
+                if (IsFall())
+                {
+                rb.velocity += transform.forward * 12.0f * Time.deltaTime;
+                rb.velocity += Vector3.up * 8.0f * Time.deltaTime;
+               }
+            }
+      //  }
+       // AGBuffer = IsGround();
 
     }
-    void RayChack()
+
+    //地上時の移動
+    void Move_Grounding()
     {
-        ray= new Ray(transform.position,Vector3.down);
-        if (Physics.Raycast(ray, out hit, ray_distance))
-        {
-            isGround = true;
-        }
-        else
-        {
-            isGround = false;
-        }
 
+    }
 
-        Debug.DrawLine(ray.origin, ray.direction * ray_distance, Color.red);//レイを可視化するやつ
-        Debug.Log(hit.collider.gameObject.name);
+    //空中時の移動
+    void Move_Frying()
+    {
 
+    }
+
+    //地上にいるかどうか判定
+    bool IsGround()
+    { 
+       return rb.velocity.y == 0.0f;
+    }
+
+    //落下中か判定
+    bool IsFall()
+    {
+        return rb.velocity.y < 0;
     }
 
 }
